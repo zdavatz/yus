4448e789284268c9c67e2b2c2ecb1b2bd79f601a
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-11-05 15:17:21 +0100

    Added Gemfile and .travis.yml for travis-ci

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "http://rubygems.org"
+
+gem 'needle'
+gem 'pg', '0.9.0'
+
+group :development, :test do
+  gem 'flexmock', '0.8.6'
+  gem 'rake'
+  gem 'minitest'
+  gem 'hoe'
+end
--- test/suite.rb
+++ test/suite.rb
@@ -3,6 +3,13 @@
 
 $: << File.dirname(File.expand_path(__FILE__))
 
+if /^1\.8/.match(RUBY_VERSION)
+  gem 'minitest'
+  require 'minitest/autorun'
+else
+  require 'test/unit'
+end
+
 Dir.foreach(File.dirname(__FILE__)) { |file|
 	require file if /^test_.*\.rb$/o.match(file)
 }
