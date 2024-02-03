7a4fb856a26ecbbc2642f417038a34709995f29f
Niklaus Giger <niklaus.giger@member.fsf.org>
2024-02-03 21:30:11 +0100

    devenv: bundle install and create valid data/yus.key

--- devenv.nix
+++ devenv.nix
@@ -2,14 +2,25 @@
 { pkgs, ... }:
 
 {
-
-  packages = [ pkgs.git pkgs.libyaml ];
+  env.GREET = "devenv";
+  packages = [ pkgs.git pkgs.libyaml ]; #  after I added pkgs.openssl here, I could no longer call devenup because of a glibc mismatch
+  # therefore I ${pkgs.openssl}/bin/openssl in the enterShell
 
   enterShell = ''
     echo This is the devenv shell for oddb2xml
     git --version
     ruby --version
     psql --version
+    OLD_YUS_CRT=`git status --porcelain data;`
+    if [[ -z $OLD_YUS_CRT ]]; then
+      echo Must replace old yus certificat from 2006
+      cd data
+      pwd
+      ${pkgs.openssl}/bin/openssl req -nodes -new -x509 -key yus.key -out yus.crt -subj "/C=CH/ST=Zurich/L=Zurich/O=ywesee GmbH/OU=IT Department CI/CN=ywesee.com"
+    else
+      echo Found changed data/yus.key
+    fi
+    bundle install
   '';
 
   languages.ruby.enable = true;
